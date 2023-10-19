ef6be6f8879918f32fdac00c71e4340b6f877985
Vadym Shevchenko <shev4enko.vadym@gmail.com>
2023-10-19 12:28:33 +0300

    feat: do not cropp elements on mobile resolution

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} overflow-hidden`} suppressHydrationWarning={true}>
+      <body className={`${inter.className} overflow-x-hidden`} suppressHydrationWarning={true}>
         <header>
           <Navbar />
         </header>
