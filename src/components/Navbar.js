a1a328ca743778578e489d991b67fc60b5fbbb19
Vadym Shevchenko <shev4enko.vadym@gmail.com>
2023-10-23 17:14:13 +0300

    feat: user editing page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@
 
 import { Navbar } from 'flowbite-react';
 import { useUser } from '@/hooks/useUser';
+import Image from 'next/image';
 
 export default function HeaderNavbar() {
   const { user, onResetUserState } = useUser();
@@ -34,20 +35,29 @@ export default function HeaderNavbar() {
         <Navbar.Link href="/user">
           {user ? (
             <div className="flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+              {user.avatarUrl ? (
+                <Image
+                  src={user.avatarUrl}
+                  width={50}
+                  height={50}
+                  alt="user avatar"
                 />
-              </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                  />
+                </svg>
+              )}
 
               <span className="ml-2 text-gray-900 dark:text-white">
                 {user.nickname}
